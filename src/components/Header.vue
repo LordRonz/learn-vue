ad7c151480459289b57932cedf36c1ef6ad23784
LordRonz <aarontanhar2000@gmail.com>
2021-03-11 15:54:44 +0700

    version 69.69.69

---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      v-show="homePage"
+      @toggle-add-task="$emit('toggle-add-task')"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
+    />
+  </header>
+</template>
+
+<script>
+import Button from "./Button";
+export default {
+  name: "Header",
+  props: {
+    title: {
+      type: String,
+      default: "Hello World",
+    },
+    showAddTask: Boolean,
+  },
+  components: {
+    Button,
+  },
+  computed: {
+    homePage() {
+      return this.$route.path === '/'
+    },
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,4 @@
+<template>
+    <h3>Version 69.69.69</h3>
+    <router-link to='/'>Go Back</router-link>
+</template>
